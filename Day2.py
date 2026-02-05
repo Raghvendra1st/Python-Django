887dede2ce4cfac54d7e03ea99a1649ea0adc90a
Raghvendra1st <164016383+Raghvendra1st@users.noreply.github.com>
2026-02-05 23:16:20 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,100 @@
+# # # # # # # # price=180
+# # # # # # # # is_member =False
+# # # # # # # # if price>=200:
+# # # # # # # #     print("10% Discout")
+# # # # # # # # else:
+# # # # # # # #     print("Nothing")
+# # # # # # # paid=True    
+# # # # # # # shipped =flase
+# # # # # # # if paid and shiped
+
+# # # # # # ''''' Nested If'''
+# # # # # # if
+# # # # # #    if 
+# # # # # #       if
+
+# # # # # ''''''  Led if ''''''
+# # # # # if
+# # # # # if
+# # # # # if
+
+# # # # '''''LIST '''
+# # # # ## Collection of Items can store Hetrogenous items , no fixed types 
+# # # # '='ASSIGNMENT Operator ,== comparison operator
+
+# # # #Tuple= value doest change 
+# # # List =[1,'a',("ab",)]
+# # # print(List)
+# fruits=["Banna","kiwi","Apple","Greps","Strwberry"]
+# # # print(fruits[1])
+# # # print(fruits[0:1])
+# # # print(fruits[::])
+# # # print(fruits[::-1])
+# # # print(fruits[0])
+# # # print(fruits[:-1])
+# # # # print(fruits[:-2])
+# # # # fruits.extend()#alue due to 
+# # # #
+# # # fruits.insert(1,"ber")
+# # # print(fruits)
+# # fruits.remove("KIWI")
+
+# print(fruits)
+fruits =["Mango","Apple","Guavva","Greps"]
+print(fruits)
+#.pop(index), .remove(),.
+"min max"
+print(max(fruits))
+print(min(fruits))
+name=["prince","priyam","priyanshu",]
+print("\n")#break Statement 
+
+print(min(name))
+print(max(name))
+print(name.count('Priyam'))
+#.clear
+name.sort()
+print(name)
+number =[2, 33,4, 5,6]
+print(number[0])
+print(number[-1])
+print(len(number))
+print(sum(number))
+avg=sum(number)/len(number)
+print(avg)
+fruitss=["APPLE","Guava","Grapes",]
+fruitss.append("Green Apple",)
+print(fruitss)
+fruitss.insert(2,"blueberry")
+print(fruitss)
+lists=[1,3,4,5,6,43,2]
+print(lists.count(1))
+
+name3=["aman ", "aaakash","abhi",]
+combine=name3+fruits
+print(combine)
+print(fruitss*2)
+
+"""""Combine"""
+#mutable
+set1 ={'1','2','34',}
+set2={'2','3','5','6','67'}
+union=set1.union(set2)
+print(union)
+print(set1.intersection(set2))
+set1.add("Ab")
+print(set1)
+set1.clear()
+print(set1)
+
+
+#.diffrece
+
+set4={"Aman ","ashish","akash",}
+set5={"Aman","raju","rajneesh"}
+print(set4.intersection(set5))
+print(set4.difference(set5))
+print(set4.add(set5))
+
+'''''Tuple '''
+# secure , more safe, data sequrity
---
+++ first.py
@@ -0,0 +1,71 @@
+# #n=40
+# #p=15
+# #total=n*2+p*3
+# ##
+
+# #num1=int(input("enter first value -"))
+# #num2=int(input("enter Second value -"))
+# #num3=int(input("enter third value -"))
+# #num4=int(input("enter fourth value -"))
+# ##num5=int(input("enter fivth value -"))
+# #avg=(num1+num2+num3+num4+num5)/5
+# #print("Avg of total-",avg)
+
+# # num1=int(input("Enter Princilpal Amount -"))
+# # num2=int(input("Enter Rste of Interest -"))
+# # num3=int(input("Enter Time -"))
+# """SimpleInterset=(num1*num2*num3)/100
+# print("The Interest of Given Amount is -",SimpleInterset )
+# """
+# """String"""#(Case Sensitive)
+# #function- , cheet sheet strName. - vscode automaticle gives Functions
+
+
+# r1="Artificial Intelligence"
+# print(str1[2:12]) # start with 2 and end with - 12 index
+# print(str1[:15])
+# print(str1[-12:-1])
+# print(str1[::-1])#reverse the String
+# print(str1[20])
+
+# #immutable - can not does item Assignm
+# name =str(input("Enter your Name -"))
+# print("My Name is -" ,name[::-1])## Reverse the String
+# print(len(name))#Length of String
+# print(name.count('r'))# Count the Accurence of World , String
+# #upper ,capatalise , 
+# print(name.capitalize())#First Letter Capatalised
+# print(name.title())
+# print(name.upper())
+
+# print(name.endswith("ra"))
+# print(name.startswith("df"))
+
+# #Array -Same Datas 
+# #List - Diffrent kind of Data Types
+# #Split
+# print(name.split())
+# #concatination
+# print(name+' ' +str1)
+# '''''Imp'''
+# #Tuple 
+# #List 
+# #Dictionaries
+# #Why Use Strings
+# #Remove Space-Strip function , Data Cleaning
+
+# print(name.rstrip())
+# print(name.replace("r","g"))#Replace
+'''''Conditional Statement '''
+'''''Pelindrome'''
+
+str1=str(input("ENTER String-"))
+str2=str1[::-1]
+if str1== str2:
+    print("Pelindron")
+else:
+    print("Pelindron")
+
+
+
+
